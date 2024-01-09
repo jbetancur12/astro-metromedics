c06467d97cc84674b44740dc3e92ced8f6f199f1
Jorge Betancur <jorge.betancur@teads.tv>
2024-01-09 16:20:58 -0500

    new updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+import { defineConfig } from 'astro/config';
 
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap(), robotsTxt(), compressor({ gzip: true, brotli: false },)],
-  site: 'http://localhost:4321',
+  site: 'https://metromedics.co',
   base: '/'
 });
