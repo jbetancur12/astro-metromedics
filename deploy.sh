c44866bdf16112bcfb44915f4e19bfb3ca394eca
Jorge Betancur <jorge.betancur@teads.tv>
2025-02-11 14:38:45 -0500

    adjustments

---
+++ deploy.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Nombre del archivo de registro de errores
+error_log="error.log"
+
+# Función para mostrar los errores
+show_errors() {
+  if [ -s "$error_log" ]; then
+    echo "Se produjeron errores. Consulta el archivo $error_log para más detalles."
+  else
+    echo "El script se ejecutó sin errores."
+  fi
+}
+
+echo "Ejecutando 'build'..."
+npm run build
+echo
+
+# Verificar que la carpeta 'dist' existe
+if [ ! -d "dist" ]; then
+  echo "La carpeta 'dist' no existe. Asegúrate de que el build se haya generado correctamente."
+  exit 1
+fi
+
+
+
+# Copiar la carpeta al servidor remoto
+echo "Copiando la carpeta 'dist' al servidor remoto..."
+scp -r "dist" metromedics@209.97.156.169:/home/metromedics/mmcs/landingpage 2>>"$error_log"
+if [ $? -ne 0 ]; then
+  echo "Error al copiar la carpeta 'dist' al servidor remoto."
+  show_errors
+  exit 1
+fi
+
+# Conectarse al servidor remoto y mover archivos al directorio de producción.
+echo "Conectándose al servidor remoto y moviendo archivos..."
+ssh metromedics@209.97.156.169 << 'EOF'
+  set -x
+  cd /home/metromedics/mmcs/landingpage
+  
+  # Obtener la fecha y hora actual
+  timestamp=$(date +"%Y%m%d-%H%M%S")
+  mv dist "build$timestamp"
+  
+  # Verificar el resultado del mv
+  if [ $? -ne 0 ]; then
+    echo "Error al mover la carpeta 'dist' al directorio de producción." >&2
+    exit 1
+  fi
+
+  sudo cp -r "build$timestamp"/* /var/www/metromedics.co/html/
+  
+  # Verificar el resultado del cp
+  if [ $? -ne 0 ]; then
+    echo "Error al copiar los archivos al directorio de producción." >&2
+    exit 1
+  fi
+
+  # Conservar solo las dos carpetas más recientes en el directorio /home/metromedics/mmcs/landingpage
+  cd /home/metromedics/mmcs/landingpage
+  # Listar los directorios ordenados por fecha de modificación (más reciente primero) y eliminar los que no sean los dos más recientes
+  dirs_to_delete=$(ls -dt */ | awk 'NR>2')
+  
+  # Eliminar los directorios que no se deben conservar
+  if [ -n "$dirs_to_delete" ]; then
+    echo "Eliminando directorios antiguos..."
+    rm -rf $dirs_to_delete
+  fi
+
+EOF
+
+# Mostrar errores
+show_errors
--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -5,92 +5,113 @@ export default function Header() {
 
   return (
     <nav
-      id='header'
-      class='fixed w-full z-30 top-0 text-white border-y-green-500 border-t-6 bg-black'
+      id="header"
+      class="fixed w-full z-30 top-0 text-white border-y-green-500 border-t-6 bg-white"
     >
-      <div
-        class='w-full container mx-auto flex items-center justify-between mt-0 py-2'
-      >
-        <div class='pl-4 flex items-center'>
+      <div class="w-full container mx-auto flex items-center justify-between mt-0 py-2">
+        <div class="pl-4 flex items-center">
           <a
-            class='toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl'
-            href='/'
+            class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+            href="/"
           >
-            <img class='max-w-[200px] md:max-w-fu mx-auto h-auto' src='/images/logo-1.png' alt='Logo' />
+            <img
+              class="max-w-[200px] md:max-w-fu mx-auto h-auto"
+              src="/images/logo-1.png"
+              alt="Logo"
+            />
           </a>
         </div>
-        <div class='block lg:hidden pr-4'>
+        <div class="block lg:hidden pr-4">
           <button
-            id='nav-toggle'
-            class='flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'
+            id="nav-toggle"
+            class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
             <svg
-              class='fill-current h-6 w-6'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
+              class="fill-current h-6 w-6"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <title>Menu</title>
-              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'></path>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
             </svg>
           </button>
         </div>
         <div
-          class='w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20'
-          id='nav-content'
+          class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+          id="nav-content"
         >
-          <ul
-            class='list-reset lg:flex justify-end flex-1 items-center text-2xl'
-          >
-            <li class='mr-3'>
+          <ul class="list-reset lg:flex justify-end flex-1 items-center text-2xl">
+            <li class="mr-3">
               <a
-                class='inline-block py-2 px-4 text-white no-underlin hover:text-green-500 hover:text-underline'
-                class: list={[
-                  Astro.url.pathname == '/' && 'text-green-500 font-bold',
+                class="inline-block py-2 px-4 text-white no-underlin hover:text-green-500 hover:text-underline"
+                class:list={[
+                  Astro.url.pathname == "/" && "text-green-500 font-bold",
                 ]}
-                href='/'>Inicio</a
+                href="/"
               >
+                Inicio
+              </a>
             </li>
-            <li class='mr-3'>
+            <li class="mr-3">
               <a
-                class='inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4'
-                class: list={[Astro.url.pathname == "/servicios" && "text-green-500 font-bold"]}
-                href='servicios'>Servicios</a
-              >
-            </li>
-            <li class='mr-3'>
-              <a
-                class='inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4'
-                href='#'>Laboratorios</a
-              >
-            </li>
-            <li class='mr-3'>
-              <a
-                class='inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4'
-                class: list={[
-                  Astro.url.pathname == '/nosotros' &&
-                  'text-green-500 font-bold',
+                class="inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4"
+                class:list={[
+                  Astro.url.pathname == "/servicios" &&
+                    "text-green-500 font-bold",
                 ]}
-                href='nosotros'>Nosotros</a
+                href="servicios"
               >
+                Servicios
+              </a>
             </li>
-            <li class='mr-3'>
+            <li class="mr-3">
               <a
-                class='inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4'
-                class: list={[Astro.url.pathname == "/contacto" && "text-green-500 font-bold"]}
-                href='contacto'>Contacto</a
+                class="inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4"
+                href="#"
               >
+                Laboratorios
+              </a>
             </li>
-            <li class='mr-3'>
+            <li class="mr-3">
               <a
-                class='inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4'
-                class: list={[Astro.url.pathname == "/evaluacion-del-servicio" && "text-green-500 font-bold"]}
-                href='contacto'>Evaluación del Servicio</a
+                class="inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4"
+                class:list={[
+                  Astro.url.pathname == "/nosotros" &&
+                    "text-green-500 font-bold",
+                ]}
+                href="nosotros"
               >
+                Nosotros
+              </a>
+            </li>
+            <li class="mr-3">
+              <a
+                class="inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4"
+                class:list={[
+                  Astro.url.pathname == "/contacto" &&
+                    "text-green-500 font-bold",
+                ]}
+                href="contacto"
+              >
+                Contacto
+              </a>
+            </li>
+            <li class="mr-3">
+              <a
+                class="inline-block text-white no-underline hover:text-green-500 hover:text-underline py-2 px-4"
+                class:list={[
+                  Astro.url.pathname == "/evaluacion-del-servicio" &&
+                    "text-green-500 font-bold",
+                ]}
+                href="contacto"
+              >
+                Evaluación del Servicio
+              </a>
             </li>
           </ul>
           <button
-            id='navAction'
-            class='mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out'
+            id="navAction"
+            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
             Clientes
           </button>
@@ -98,5 +119,5 @@ export default function Header() {
       </div>
       {/* <!-- <hr class="border-b border-gray-100 opacity-25 my-0 py-0" /> --> */}
     </nav>
-  )
+  );
 }
